c771d61e9744eede146e83333c05fe63dff42bde
Eddie-boi <edvard.thornros@gmail.com>
2017-04-13 12:45:21 +0200

    Added a paralax renderer

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,7 +1,12 @@
 package game;
+import game.shaders.GameShaders;
 import sk.audio.AudioManager;
 import sk.game.Game;
 import sk.game.GameProperties;
+import sk.gfx.Vertex2D;
+import sk.physics.Body;
+import sk.physics.Shape;
+import sk.util.vector.Vector2f;
 import sk.util.vector.Vector4f;
 
 public class Main {
@@ -12,10 +17,12 @@ public class Main {
 		gp.clearColor = new Vector4f();
 		gp.width = 800;
 		gp.height = 600;
-		gp.startState = TG.GS_MAIN_MENU;
+		gp.startState = TG.GS_PARALLAX_TEST;
 		gp.resizable = false;
 		gp.title = "Time Game";
 		gp.vSync = true;
+		
+		//Body b = new Body(new Shape(Vector2f(0.0f, 0.0f), Vector2f(0.0f, 0.0f)));
 		
 		System.out.println("Starting game...");
 		
@@ -23,7 +30,14 @@ public class Main {
 		
 		Game.start(gp);
 		
+		GameShaders.destroyShaders();
+		
 		System.out.println("Game successfully exited");
+	}
+
+	private static Vertex2D Vector2f(float f, float g) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
--- src/game/TG.java
+++ src/game/TG.java
@@ -6,5 +6,6 @@ public class TG {
 	
 	// Gamestates
 	public static final MainMenu GS_MAIN_MENU = new MainMenu();
+	public static final Parallax GS_PARALLAX_TEST = new Parallax();
 	
 }
