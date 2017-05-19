49d4d314b4445961f66688d247ab32839b618308
Eddie-boi <edvard.thornros@gmail.com>
2017-05-19 16:25:48 +0200

    Refactored the menu, a lot...

--- src/game/state/MainMenu.java
+++ src/game/state/MainMenu.java
@@ -2,100 +2,188 @@ package game.state;
 
 import java.awt.Font;
 
+import com.sun.org.apache.bcel.internal.generic.GOTO;
+
 import game.TG;
 import sk.entity.Entity;
 import sk.game.Game;
+import sk.game.Window;
 import sk.gamestate.GameState;
 import sk.gamestate.GameStateManager;
 import sk.gfx.Texture;
 import sk.gfx.gui.GUIButton;
+import sk.gfx.gui.GUIElement;
 import sk.gfx.gui.GUIText;
 import sk.sst.SST;
 
 import sk.util.vector.Vector4f;
 
 public class MainMenu implements GameState {
+
+	enum MenuState {
+		MAIN_MENU,
+		SETTINGS,
+		CREDITS,
+	}
 	
-	private Entity num[];
+	private Entity buttons[];
+	private GUIElement logo;
 	
-	public void menu (){
-		num = new Entity[4];
-		for(int x = 0; x < num.length; x++){
-			num[x]= new Entity();
-			num[x].add(new GUIButton(0f,0f,0,-60*x,200,50));
-			num[x].get(GUIButton.class).setText(new GUIText("Hej", 50, 25, Font.getFont(Font.MONOSPACED)));
-			num[x].get(GUIButton.class).setTexture(Texture.DEFAULT);
-			num[x].get(GUIButton.class).setHue(new Vector4f(1f,1f,1f,.5f));
-			
-			num[x].get(SST.class).store("index", x);
-			
-			/*
-			num[x].get(GUIButton.class).setOnHover((sst) -> {
-				num[(int) sst.get("index")].get(GUIButton.class).setHue(COLOR_ON);
-					 
-			});
-			
-			num[x].get(GUIButton.class).setOnUnhover((sst) -> {
-				num[(int) sst.get("index")].get(GUIButton.class).setHue(COLOR_OFF);
-				 
-			});
-			*/
+	private MenuState state;
+
+	public static final int BUTTON_SPACING = -60;
+
+	public static final int TEXT_WIDTH  = 150;
+	public static final int TEXT_HEIGHT = 50;
+
+	public static final int WIDTH  = 150;
+	public static final int HEIGHT = 50;
+
+	public static final Font MENU_FONT = new Font(Font.SANS_SERIF, 0, 25);
+
+	public static final Vector4f DEFAULT_TEXT_COLOR = new Vector4f(0, 1, 0, 1);
+	public static final Vector4f HOVER_TEXT_COLOR = new Vector4f(0, 1, 1, 1);
+
+	public static final Vector4f DEFAULT_COLOR = new Vector4f(0, 0, 1, 1);
+	public static final Vector4f HOVER_COLOR = new Vector4f(1, 0, 0, 1);
+
+	public static Texture LOGO;
+
+	public void generateButtons(int offsetX, int offsetY, String... names) {
+		if (names.length == 0) {
+			buttons = null;
+			return;
 		}
 		
-		num[0].get(GUIButton.class).setText(new GUIText("START", 50, 25, Font.getFont(Font.MONOSPACED)));
-		num[1].get(GUIButton.class).setText(new GUIText("CREDITS", 50, 25, Font.getFont(Font.MONOSPACED)));
-		num[2].get(GUIButton.class).setText(new GUIText("KEYS", 50, 25, Font.getFont(Font.MONOSPACED)));
-		num[3].get(GUIButton.class).setText(new GUIText("EXIT", 50, 25, Font.getFont(Font.MONOSPACED)));
+		buttons = new Entity[names.length];
+		for (int i = 0; i < names.length; i++) {
+			String name = names[i];
+			buttons[i] = generateButton(offsetX, offsetY + BUTTON_SPACING * i, name, i);
+		}
+	}
 	
+	public Entity generateButton(int offsetX, int offsetY, String name, int index) {
+		GUIButton button = new GUIButton(0, 0, offsetX, offsetY, WIDTH, HEIGHT);
+		Entity e = new Entity();
+		e.add(button);
+		e.get(SST.class).store("index", index);
+
+		button.setTexture(Texture.DEFAULT);
+		button.setText(new GUIText(name, TEXT_WIDTH, TEXT_HEIGHT, MENU_FONT, DEFAULT_TEXT_COLOR));
+		button.setHue(DEFAULT_COLOR);
+		button.setOnHover((sst) -> {
+			int i = (int) sst.get("index");
+			buttons[i].get(GUIButton.class).setHue(HOVER_COLOR);
+			buttons[i].get(GUIButton.class).getText().setColor(HOVER_TEXT_COLOR);
+		});
+
+		button.setOnUnhover((sst) -> {
+			int i = (int) sst.get("index");
+			buttons[i].get(GUIButton.class).setHue(DEFAULT_COLOR);
+			buttons[i].get(GUIButton.class).getText().setColor(DEFAULT_TEXT_COLOR);
+		});
+		return e;
+	}
+	
+	public void enterMain() {
+		state = MenuState.MAIN_MENU;
 		
-		((GUIButton) num[0].get(GUIButton.class)).setOnClick((sst) -> {
+		generateButtons(0, -50, "Start", "Credits", "Settings", "Exit");
+		
+		((GUIButton) buttons[0].get(GUIButton.class)).setOnClick((sst) -> {
 			GameStateManager.enterState(TG.GS_CHAPTER_MENU);
 		});
-		
-		((GUIButton) num[1].get(GUIButton.class)).setOnClick((sst) -> {
-			System.out.println("Insert Credits screen");
-		});	
-		
-		((GUIButton) num[2].get(GUIButton.class)).setOnClick((sst) -> {
-			System.out.println("Insert options screen");
-		});	
-		
-		((GUIButton) num[3].get(GUIButton.class)).setOnClick((sst) -> {
+
+		((GUIButton) buttons[1].get(GUIButton.class)).setOnClick((sst) -> {
+			enterCredits();
+		});
+
+		((GUIButton) buttons[2].get(GUIButton.class)).setOnClick((sst) -> {
+			enterSettings();
+		});
+
+		((GUIButton) buttons[3].get(GUIButton.class)).setOnClick((sst) -> {
 			Game.stop();
 		});
-	} 
+	}
 	
+	public void enterCredits() {
+		state = MenuState.CREDITS;
+		
+		buttons = new Entity[1];
+		buttons[0] = generateButton(0, - (2 *  BUTTON_SPACING + HEIGHT / 2), "Back", 0);
+		buttons[0].get(GUIButton.class).setAnchor(0, -1);
+		buttons[0].get(GUIButton.class).setOnClick((sst) -> {
+			enterMain();
+		});
+	}
 	
-	
-	
+	public void enterSettings() {
+		state = MenuState.SETTINGS;
+		
+		generateButtons(2 * BUTTON_SPACING, 0, "Toggle Fullscreen", "Sound Effects Volume", "Music Volume", "Back");
+		
+		buttons[0].get(GUIButton.class).setOnClick((sst) -> {
+			System.out.println("I am fullscreen: " + Window.isFullscreen());
+			if (!Window.isFullscreen()) {
+				Window.enterBorderless();
+			} else {
+				Window.enterFloating(0, 0, Window.getWidth(), Window.getHeight());
+			}
+		});
+		
+		buttons[1].get(GUIButton.class).setOnClick((sst) -> {
+			System.out.println("WOOOO SOUND EFFECTS!");
+		});
+		
+		buttons[2].get(GUIButton.class).setOnClick((sst) -> {
+			System.out.println("WOOOO MUSIC!");
+		});
+		
+		buttons[3].get(GUIButton.class).setOnClick((sst) -> {
+			enterMain();
+		});
+	}
+
 	@Override
 	public void init() {
-		menu();
+		// Only load the logo once
+		LOGO = new Texture("res/texture/temp.png");
+		logo = new GUIElement(0, 1, 0, -150, 300, 300);
+		logo.setText(new GUIText("", 0, 0, MENU_FONT));
+		logo.setTexture(LOGO);
+		
+		enterMain();
 	}
-	
+
 	@Override
 	public void update(double delta) {
-		
-		for(int x=0;x<num.length;x++){
-			num[x].update(delta);
+		for (int x = 0; x < buttons.length; x++) {
+			buttons[x].update(delta);
 		}
 	}
-	
+
 	@Override
 	public void draw() {
-			
-		for(int x=0;x<num.length;x++){
-			num[x].draw();
+		switch (state) {
+		case MAIN_MENU:
+			logo.draw();
+			break;
+		case CREDITS:
+			break;
+		case SETTINGS:
+			break;
+		}
+
+		for (int i = 0; i < buttons.length; i++) {
+			buttons[i].draw();
 		}
 	}
-	
+
 	@Override
 	public void exit() {
-		for(Entity e : num) {
+		for (Entity e : buttons) {
 			e.destroy();
 		}
 	}
-	
-	public static final Vector4f COLOR_ON = new Vector4f(0f,1f,0f,.5f);
-	public static final Vector4f COLOR_OFF = new Vector4f(1f,1f,1f,.5f);
 }
